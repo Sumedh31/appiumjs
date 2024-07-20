b4f875486bf86ebf790412da0e016219419c4b44
Sumedh Rajiv Tambe <sumedhrtv@gmail.com>
2024-07-20 17:58:08 +0100

    Generate Allure reports on finishing

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -66,7 +66,10 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['allure', { outputDir:'allure-results', disableWebdriverStepsReporting: true, disableWebdriverScreenshotsReporting: false }]
+    ],
     // Options to be passed to Mocha.
     mochaOpts: {
         ui: 'bdd',
