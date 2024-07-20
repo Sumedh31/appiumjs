00059a7e272d52826cf5ef5a1d2803e8263353d5
Sumedh Rajiv Tambe <sumedhrtv@gmail.com>
2024-07-20 19:03:32 +0100

    Updates

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -26,51 +26,17 @@ export const config: Options.Testrunner = {
     // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'debug',
     bail: 0,
-    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-    // gets prepended directly.
     baseUrl: 'http://localhost',
-    // Default timeout for all waitFor* commands.
-    /**
-     * NOTE: This has been increased for more stable Appium Native app
-     * tests because they can take a bit longer.
-     */
     waitforTimeout: 45000,
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
     connectionRetryTimeout: 120000,
     // Default request retries count
     connectionRetryCount: 3,
-    // Test runner services
-    // Services take over a specific job you don't want to take care of. They enhance
-    // your test setup with almost no effort. Unlike plugins, they don't add new
-    // commands. Instead, they hook themselves up into the test process.
     services: [],
-    // Framework you want to run your specs with.
-    // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: https://webdriver.io/docs/frameworks
-    //
-    // Make sure you have the wdio adapter package for the specific framework installed
-    // before running any tests.
     framework: 'mocha',
-    // The number of times to retry the entire spec file when it fails as a whole
-    // specFileRetries: 1,
-    //
-    // Delay in seconds between the spec file retry attempts
-    // specFileRetriesDelay: 0,
-    //
-    // Whether or not retried spec files should be retried immediately or deferred to the end of the queue
-    // specFileRetriesDeferred: false,
-    //
-    // Test reporter for stdout.
-    // The only one supported by default is 'dot'
-    // see also: https://webdriver.io/docs/dot-reporter
     reporters: [
         'spec',
         ['allure', { outputDir:'allure-results', disableWebdriverStepsReporting: true, disableWebdriverScreenshotsReporting: false }]
     ],
-    // Options to be passed to Mocha.
     mochaOpts: {
         ui: 'bdd',
         /**
